d4116fad1ddfa6fea821498a1c36b65b3c759c4b
r-arun <rathakrishnanarun@gmail.com>
2013-03-10 16:32:11 -0500

    Added Job

---
+++ hw1/job.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#$ -V
+#$ -cwd
+#$ -q development
+#$ -pe 12way 12			
+#$ -N a
+#$ -o output_a
+#$ -e error_a
+#$ -M rathakrishnanarun@gmail.com
+#$ -m be
+#$ -l h_rt=01:00:00
+
+
+export PATH=$PATH:$HOME/cilk/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$HOME/cilk/lib
+./a.out test_graph.in 0 0 0 0 > serial_bfs_output
