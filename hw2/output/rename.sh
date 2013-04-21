305edf76a1b4dd87cf7be3bfa1d41a4d4f333533
r-arun <rathakrishnanarun@gmail.com>
2013-04-20 23:44:24 -0500

    Final HW2

---
+++ hw2/output/rename.sh
@@ -0,0 +1,10 @@
+mv as-skitter-2j-CC-2-out.txt as-skitter-2j-CC-3-out.txt
+mv com-amazon-2j-CC-2-out.txt com-amazon-2j-CC-3-out.txt
+mv com-dblp-2j-CC-2-out.txt com-dblp-2j-CC-3-out.txt
+mv com-lj-2j-CC-2-out.txt com-lj-2j-CC-3-out.txt
+mv com-orkut-2j-CC-2-out.txt com-orkut-2j-CC-3-out.txt
+mv parallel_com-ca-2j-CC-2-out.txt parallel_com-ca-2j-CC-3-out.txt
+mv roadNet-CA-2j-CC-2-out.txt roadNet-CA-2j-CC-3-out.txt
+mv roadNet-PA-2j-CC-2-out.txt roadNet-PA-2j-CC-3-out.txt
+mv roadNet-TX-2j-CC-2-out.txt roadNet-TX-2j-CC-3-out.txt
+
